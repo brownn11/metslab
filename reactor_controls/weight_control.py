3013ae51968ba9c4df26c4f4dc9ffd18e01d40f5
Nina Brown <103523018+brownn11@users.noreply.github.com>
2023-08-18 16:14:09 -0500

    Update weight_control.py

--- reactor_controls/weight_control.py
+++ reactor_controls/weight_control.py
@@ -8,12 +8,7 @@ import serial.tools.list_ports as ports
 from reactor_controls import instrument_controls as ic
 
 #----------------------------------------------------------------------------------------------------------------
-# Abiltiy to open/close valves:
-def setvalve(ser, pinid, cmd):  # modify for valve opening and closing - 
-    ser.write(atob(pinid + ','+ cmd + '\x0D')) 
-    return #valveset
-#----------------------------------------------------------------------------------------------------------------
-# Open/close valves based on weight readings:
+# Open/close valves using an Arduino computer based on weight readings:
 def weight_control(portIDV = '/dev/cu.usbmodem2101',portIDW = '/dev/cu.usbmodem2101',runtime=60,checktime=10,wgtlim=25):
     #serial port setup for valves
     serV = serial.Serial(portIDV) 
@@ -32,8 +27,8 @@ def weight_control(portIDV = '/dev/cu.usbmodem2101',portIDW = '/dev/cu.usbmodem2
     cmd = True
 
     print('--> 10 = infuse; 12 = withdraw \n--> starting: port %s : %s and port %s : %s' %(ii,int(cmd),ii+2,int(cmd)))
-    setvalve(serV, str(ii), str(int(cmd)))
-    setvalve(serV, str(ii+2), str(int(not cmd)))
+    ic.setvalve(serV, str(ii), str(int(cmd)))
+    ic.setvalve(serV, str(ii+2), str(int(not cmd)))
     # loop pumping over a given time
     while (time.time()-tini)<=runtime: # set run time
         if (time.time()-tstp)>=checktime: # checks weight
@@ -42,17 +37,17 @@ def weight_control(portIDV = '/dev/cu.usbmodem2101',portIDW = '/dev/cu.usbmodem2
             if abs(wini-wnow)>=wgtlim: # switches direction after meeting 25g change
                 # close the open valve:
                 cmd = not cmd
-                setvalve(serV, str(ii), str(int(cmd)))
+                ic.setvalve(serV, str(ii), str(int(cmd)))
                 print('--> set port %s to %s'%(ii,cmd))
                 # switch to other valve and open:
                 ii+=jj
                 cmd = not cmd
-                setvalve(serV, str(ii), str(int(cmd)))
+                ic.setvalve(serV, str(ii), str(int(cmd)))
                 print('--> set port %s to %s'%(ii,cmd))
                 jj*=-1
             tstp=time.time()
 
     # close both when finished
-    setvalve(serV, '10', '0')
-    setvalve(serV, '12', '0')
+    ic.setvalve(serV, '10', '0')
+    ic.setvalve(serV, '12', '0')
     print('Finished at', time.time()-tini,'s')
